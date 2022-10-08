176f38aa940d004a5d5a97d708f2de639ba035e6
cail <cail1844@gmail.com>
2022-10-08 13:36:58 +0800

    [fix] date filed set null and blank

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -32,7 +32,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField('Author')
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
-    publication_date = models.DateField(auto_now_add=True)
+    publication_date = models.DateField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.title
